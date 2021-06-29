a31d759ab38d6ccbf66e61a35a1b3c3ad3e71127
Vi Le Truong <vi.le@executionlab.asia>
2021-06-29 19:26:11 +0700

    fix event

---
+++ src/app/modules/group/components/about-event/about-event.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AboutEventComponent } from './about-event.component';
+
+describe('AboutEventComponent', () => {
+  let component: AboutEventComponent;
+  let fixture: ComponentFixture<AboutEventComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AboutEventComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AboutEventComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
